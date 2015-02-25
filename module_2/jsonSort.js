fc4210f8f3da9f526efe5453d1b99222c6e72142
nileshpharande88 <nilesh.pharande@mindstix.com>
2015-02-25 15:13:46 +0530

    added module_3 and respective code in test_Assign_2 project.

--- module_2/jsonSort.js
+++ module_2/jsonSort.js
@@ -9,6 +9,14 @@ function sortJSON(jsonObject, callback){
 		//checks for the presence of students inan array of the students tag.
 		else if (students.length == 0) throw new Error( " jsonObject not contain students in array." );
 
+		//checks for the presence of elements of each object.
+		students.forEach(function (value) {
+	    	if( (value.id === undefined) ) throw new Error( " id is not found in object" );
+	    	else if( (value.fName === undefined) ) throw new Error( " fName is not found in object" );
+	    	else if( (value.lName === undefined) ) throw new Error( " lName is not found in object" );
+	    	else if( (value.score === undefined) ) throw new Error( " score is not found in object" );
+    	});
+
 		//actual sorting of array elements
 		students.sort( function (a, b) {
 	        return b.score - a.score ;
--- test_Assign_2/test_Assign_2.js
+++ test_Assign_2/test_Assign_2.js
@@ -1,12 +1,37 @@
 var jsonReader = require("json-reader");
 var jsonSort = require("json-sort");
+var async = require("async");
+var jsonToText = require("json-to-text");
+
+if ((jsonReader === undefined)) throw new Error( " Can't access jsonReader module" );
+if ((jsonSort === undefined)) throw new Error( " Can't access jsonSort module" );
+if ((async === undefined)) throw new Error( " Can't access async module" );
 
 jsonReader.jsonObject("source.json", function(err,object){
 	if(err) console.log(object);
 	else {
 		jsonSort.sortJSON(object,function(err,sortedStudentArray){
 			if(err) console.log(err);
-			else console.log(sortedStudentArray);
+			else {
+				async.series( [
+					function(callback){
+						jsonToText.TextFileCreator("destination.txt", sortedStudentArray, function(err, res){
+							 if(err){
+							 	console.log("Failed: ", res);
+							 	callback(1, 'one failed');
+							 }else{
+							 	console.log("Successful: ", res);
+							 	callback(null, 'one success');
+							 }
+						});
+					},
+					function(callback){
+						callback(null, 'one success');
+					}
+				], function(err, results){
+					console.log("Final Result: " + results);
+				});
+			}
 		});
 	}
 });
