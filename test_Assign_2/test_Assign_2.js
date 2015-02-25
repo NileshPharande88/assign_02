b9185957cfb9e1d8279983c8e8d2c48a9fcc3a4d
nileshpharande88 <nilesh.pharande@mindstix.com>
2015-02-25 19:15:08 +0530

    Added module_4 and respective code in the test_Assign_2 project is also added.

--- test_Assign_2/test_Assign_2.js
+++ test_Assign_2/test_Assign_2.js
@@ -2,9 +2,12 @@ var jsonReader = require("json-reader");
 var jsonSort = require("json-sort");
 var async = require("async");
 var jsonToText = require("json-to-text");
+var jsonToXML = require("json-to-xml");
 
-if ((jsonReader === undefined)) throw new Error( " Can't access jsonReader module" );
-if ((jsonSort === undefined)) throw new Error( " Can't access jsonSort module" );
+if ((jsonReader === undefined)) throw new Error( " Can't access json-reader module" );
+if ((jsonSort === undefined)) throw new Error( " Can't access json-sort module" );
+if ((jsonToText === undefined)) throw new Error( " Can't access json-to-text module" );
+if ((jsonToXML === undefined)) throw new Error( " Can't access json-to-xml module" );
 if ((async === undefined)) throw new Error( " Can't access async module" );
 
 jsonReader.jsonObject("source.json", function(err,object){
@@ -18,7 +21,7 @@ jsonReader.jsonObject("source.json", function(err,object){
 						jsonToText.TextFileCreator("destination.txt", sortedStudentArray, function(err, res){
 							 if(err){
 							 	console.log("Failed: ", res);
-							 	callback(1, 'one failed');
+							 	callback(null, 'one failed');
 							 }else{
 							 	console.log("Successful: ", res);
 							 	callback(null, 'one success');
@@ -26,7 +29,15 @@ jsonReader.jsonObject("source.json", function(err,object){
 						});
 					},
 					function(callback){
-						callback(null, 'one success');
+						jsonToXML.XMLFileCreator("destination.xml", sortedStudentArray, function(err, res){
+							 if(err){
+							 	console.log("Failed: ", res);
+							 	callback(null, 'two failed');
+							 }else{
+							 	console.log("Successful: ", res);
+							 	callback(null, 'two success');
+							 }
+						});
 					}
 				], function(err, results){
 					console.log("Final Result: " + results);
